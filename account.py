c8d8a93ef2e90577a5ef272d1f3f50dca02fd85a
aditya219059 <96041584+aditya219059@users.noreply.github.com>
2021-12-13 12:29:54 +0530

    Add files via upload

---
+++ account.py
@@ -0,0 +1,10 @@
+n=input("Enter the name:")
+acc=int(input("Enter the account number:"))
+w=int(input("Enter the amount to be withdrawn:"))
+bal=12000
+if bal>w or bal>w:
+    print("***Your withdrawal is successful***")
+    print("left account balance:",bal-w)
+else:
+    print("***ERROR***")
+    print("***Insufficient Amount***")
---
+++ age.py
@@ -0,0 +1,4 @@
+n=input("Enter the name:")
+a=int(input("Enter the age:"))
+y=2021+100-a
+print("The year in which you will become 100 years old:",y)
