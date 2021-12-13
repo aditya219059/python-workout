1ccafcefca8da99bf284fc55a5aa238a567ea5b6
aditya219059 <96041584+aditya219059@users.noreply.github.com>
2021-12-13 12:23:27 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,18 @@
+name=input("enter your name:")
+p=int(input("enter your obtained percent:"))
+if (p<101 and p>90):
+    print("grade obtained by ",name," is A+")
+elif (p<91 and p>80):
+    print("grade obtained by ",name,"is A")
+elif (p<81 and p>70):
+    print("grade obtained by ",name," is B")
+elif (p<71 and p>60):
+    print("grade obtained by ",name," is C")
+elif s(p<61 and p>50):
+    print("grade obtained by ",name," is D")
+elif (p<51 and p>40):
+    print("grade obtained by ",name," is E")
+else:
+    print("grade obtained by ",name," is F")
+    
+
